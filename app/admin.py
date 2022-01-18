e25082ea1e62fffc5b33ad07994234a1e2b9751b
Anthonystark9 <86077320+Anthonystark9@users.noreply.github.com>
2022-01-18 20:15:02 +0530

    Add files via upload

---
+++ app/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+#from .models import Architect_table
+#from .models import Contractor_table
+#from .models import Interior_Designer_table
+
+# Register your models here.
+
+#admin.site.register(Architect_table)
+#admin.site.register(Contractor_table)
+#admin.site.register(Interior_Designer_table)
+
+
